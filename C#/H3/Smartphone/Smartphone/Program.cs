18f64888c913093f14b3e02ec54f96dd7a273a97
larsvermeulen-source <larsvermeulen@sfevergem.be>
2026-01-08 12:45:59 +0100

    smarthphone

---
+++ C#/H3/Smartphone/Smartphone/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Smartphone
+{
+    private String _telnummer;
+    private String _pincode;
+    public Smartphone(String telnummer, String pincode)
+    {
+        _telnummer = telnummer;
+        _pincode = pincode;
+    }
+    // definieer hier de Property Telnummer
+    public String Telnummer
+    {
+        get { return _telnummer; }
+        set { _telnummer = value; }
+    }
+    // definieer hier de Property Pincode
+    public String Pincode
+    {
+        get { return _pincode; }
+        set { _pincode = value; }
+    }
+    static void Main(string[] args)
+    {
+        Smartphone dimGSM = new Smartphone("nummer", "1234");
+        dimGSM.Telnummer = "0467046154";
+        dimGSM.Pincode = "8954";
+        Console.WriteLine(dimGSM.Pincode);
+        Console.WriteLine(dimGSM.Telnummer);
+        Console.ReadLine();
+    }
+}
+
+
