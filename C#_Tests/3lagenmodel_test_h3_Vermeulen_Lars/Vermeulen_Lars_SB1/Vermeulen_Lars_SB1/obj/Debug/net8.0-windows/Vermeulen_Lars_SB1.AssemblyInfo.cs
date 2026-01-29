17348460fd300e09a1f50dcc0af26dd2565e3f95
larsvermeulen-source <larsvermeulen@sfevergem.be>
2026-01-29 12:34:18 +0100

    Test 3 lagenmodel

--- C#_Tests/3lagenmodel_test_h3_Vermeulen_Lars/Vermeulen_Lars_SB1/Vermeulen_Lars_SB1/obj/Debug/net8.0-windows/Vermeulen_Lars_SB1.AssemblyInfo.cs
+++ C#_Tests/3lagenmodel_test_h3_Vermeulen_Lars/Vermeulen_Lars_SB1/Vermeulen_Lars_SB1/obj/Debug/net8.0-windows/Vermeulen_Lars_SB1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Vermeulen_Lars_SB1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b49ae8f202be2f68f17440bb7ac935fef280fca7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38401b0877ca44096c0a022f074135919d2fc355")]
 [assembly: System.Reflection.AssemblyProductAttribute("Vermeulen_Lars_SB1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Vermeulen_Lars_SB1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
